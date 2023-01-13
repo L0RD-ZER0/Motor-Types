abc4007bd5ad34a2129ad813eaa50d3839d0ddad
Zero <ackerhon@gmail.com>
2023-01-13 21:14:03 +0530

    Added type-hints for `AgnosticGridOut`
    Added the required type hints for the aformentioned class and left a placeholder for `tornado.web.RequestHandler` in method `stream_to_handler`.

--- motor/motor_gridfs.pyi
+++ motor/motor_gridfs.pyi
@@ -1,16 +1,29 @@
 import datetime
 from asyncio import AbstractEventLoop
+from os import SEEK_SET as _SEEK_SET
 from types import TracebackType
-from typing import Any, Iterable, NoReturn, Optional, Type
+from typing import (
+    Any,
+    Awaitable,
+    Dict,
+    Iterable,
+    List,
+    Mapping,
+    NoReturn,
+    Optional,
+    Type,
+    Union,
+)
 
-from gridfs.grid_file import GridIn
+from gridfs.grid_file import GridIn, GridOut
 from pymongo.client_session import ClientSession
-from typing_extensions import Literal, Self
+from typing_extensions import Literal, Protocol, Self
 
 from .core import AgnosticCollection, AgnosticCursor
 from .metaprogramming import ReadOnlyProperty
 
 _IO_Loop = AbstractEventLoop
+_Handler = Any  # Replace it with tornado.web.RequestHandler
 
 class MotorGridOutProperty(ReadOnlyProperty):
     pass
@@ -18,7 +31,42 @@ class MotorGridOutProperty(ReadOnlyProperty):
 class AgnosticGridOutCursor(AgnosticCursor):
     def next_object(self) -> Optional[AgnosticGridOut]: ...
 
-class AgnosticGridOut(object): ...
+class AgnosticGridOut(object):
+    _id: Any
+    aliases: Optional[List[str]]
+    chunk_size: int
+    content_type: Optional[str]
+    filename: Optional[str]
+    length: int
+    metadata: Optional[Mapping[str, Any]]
+    name: Optional[str]
+    upload_date: datetime.datetime
+
+    def __init__(
+        self,
+        root_collection: AgnosticCollection,
+        file_id: Optional[int] = None,
+        file_document: Optional[Any] = None,
+        delegate: GridOut = None,
+        session: ClientSession = None,
+        **kwargs: Any,
+    ) -> None: ...
+    async def __aiter__(self) -> Self: ...
+    async def __anext__(self) -> bytes: ...
+    def __getattr__(self, name: str) -> Any: ...
+    async def _ensure_file(self) -> None: ...
+    async def close(self) -> None: ...
+    def get_io_loop(self) -> _IO_Loop: ...
+    async def open(self) -> Self: ...
+    def read(self, size: int = -1) -> bytes: ...
+    def readable(self) -> Literal[True]: ...
+    async def readchunk(self) -> bytes: ...
+    def readline(self, size: int = -1) -> bytes: ...
+    def seek(self, pos: int, whence: int = _SEEK_SET) -> int: ...
+    def tell(self) -> int: ...
+    def seekable(self) -> Literal[True]: ...
+    async def write(self, data: Any) -> None: ...
+    async def stream_to_handler(self, request_handler: _Handler) -> None: ...
 
 class AgnosticGridIn(object):
     _id: Any
