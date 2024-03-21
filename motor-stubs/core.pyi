7bc1c79e0c3783f125b7d807c514a41e420b2247
antonshipelev <antonship12345@gmail.com>
2024-03-21 22:21:16 +0300

    fix with_options

--- motor-stubs/core.pyi
+++ motor-stubs/core.pyi
@@ -386,14 +386,14 @@ class AgnosticDatabase(AgnosticBaseProperties):
     ) -> AgnosticChangeStream: ...
 
     def with_options(
-            self,
+            self: _Self,
             codec_options: typing.Optional[
                 bson.codec_options.CodecOptions[typing.Any]
             ] = None,
             read_preference: typing.Optional[_ReadPreferences] = None,
             write_concern: typing.Optional[pymongo.write_concern.WriteConcern] = None,
             read_concern: typing.Optional[pymongo.read_concern.ReadConcern] = None,
-    ) -> Self: ...
+    ) -> _Self: ...
 
 
 class AgnosticCollection(AgnosticBaseProperties):
